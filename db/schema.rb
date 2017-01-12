2dbab239a3342cee6c1ffe0141313ff499b2f07f
Mark McKellar <mpm1@ualberta.ca>
2017-01-12 11:11:52 -0700

    Added Forms table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170112174745) do
+ActiveRecord::Schema.define(version: 20170112175916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "forms", force: :cascade do |t|
+    t.integer  "schema_id",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "schemas", force: :cascade do |t|
     t.string   "name"
@@ -34,4 +40,5 @@ ActiveRecord::Schema.define(version: 20170112174745) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "forms", "schemas"
 end
