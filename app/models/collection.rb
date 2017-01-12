19b9b17bab762a184d203b4fb68b103c20a2ce20
Mark McKellar <mpm1@ualberta.ca>
2017-01-12 15:08:56 -0700

    Fixed up models and added an simple test page.

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,6 +2,6 @@ class Collection < ApplicationRecord
 	belongs_to :form
 	belongs_to :owner, class_name: "User"
 	belongs_to :parent_collection, class_name: "Collection"
-	has_many :collections
+	has_many :collections, :foreign_key => "parent_collection_id"
 	has_and_belongs_to_many :items
 end
