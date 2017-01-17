8d62cca4b86268b1ac152b2851368346571369ee
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2017-01-17 15:49:44 -0700

    Schemas can now be saved.

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,5 +1,4 @@
 class Field < ApplicationRecord
-	belongs_to :form
-	
+	belongs_to :form	
 	enum type: [:text_field, :text_area, :file, :image, :email, :url, :number] unless instance_methods.include? :type
 end
