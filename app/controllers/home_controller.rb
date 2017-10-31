9450dde5df0c6d38cd2eaa401b92a9278202eaa9
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2017-10-31 10:33:49 -0600

    Root collections can be found with scope
    Root collections can now be found using the scope without_parents. The home page now lists these collections

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
-  def index
-  end
+	before_action :set_root_collections, only: [:index]
+
+	def index
+	end
+
+
+	private 
+		def set_root_collections
+			@root_collections = Collection.without_parents.where is_public: true
+		end
 end
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,4 +3,8 @@ class Collection < Entity
 	has_many :child_entities, class_name: "ParentEntity", foreign_key: :child_entity_id
 	has_many :children, :through => :child_entities, :source => :parent_entity
 
+	scope :without_parents, -> {
+		left_outer_joins(:parent_entities).where(parent_entities: {id: nil})
+	}
+
 end
