c87cfd964fc4ce3212aef309eb04d6dbb6234234
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2017-11-08 11:40:14 -0700

    Improve tree list on Collection edit view

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -6,7 +6,7 @@ class Entity < ApplicationRecord
 	has_many :parent_entities, class_name: "ParentEntity", foreign_key: :parent_entity_id
 	has_many :parents, :through => :parent_entities, :source => :child_entity
 	accepts_nested_attributes_for :metadata_values, allow_destroy: true
-	attr_accessor :children_associations
+	# attr_accessor :children_associations
 
 	def item_type_values
 		ItemType.item_type_values(self).first
